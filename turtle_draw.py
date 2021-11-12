cd46f7bdb871e528a731cc53f2fbe0240df48d01
Mike Samuels <msamuels59@gmail.com>
2021-11-12 17:07:08 -0500

    Updated

--- turtle_draw.py
+++ turtle_draw.py
@@ -1,1 +1,9 @@
 # Instructions for this exercise are here https://realpython.com/beginners-guide-python-turtle/
+print("This is my turtle program")
+import turtle
+turtle.title("My Turtle Program")
+turtle.bgcolor("blue")
+t = turtle.Turtle()
+t.fillcolor("white")
+t.circle(90)
+turtle.mainloop()
