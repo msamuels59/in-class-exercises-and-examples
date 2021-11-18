749a4df3bd6cd78a1fd0c9a09b72f5350129dce3
Mike Samuels <msamuels59@gmail.com>
2021-11-18 16:32:46 -0500

    updated

--- horse.py
+++ horse.py
@@ -12,10 +12,8 @@ or misses a given shot.'''
 
 # What variables will you need for this program? In other words, what values
 # do you need to keep track of? 
-# player 1 and player 2, letters, making shot or missing, blank list that collect letters
-#c
+
 # What functions will you need? What, if any, input will each function require? 
-#
 # What, if any, output will the function return? What, if any side effects will
 # the function have? For example, will any values be changed?
 
@@ -23,14 +21,6 @@ or misses a given shot.'''
 # what you are going to do. For example, you could do something like:
 # def shoot(player):
 #   determines whether player makes or misses shot
-#   
 #   updates players letters if missed
 
 # I will hit up each group individually after round one and get you set for round 2.
-
-# Make a HORSE list and a blank list for each player. if miss shot they get a letter form the HORSE list
-# moved down the list adding a letter skipping over ones that are present in the list
-# game keep playing until one player has a full list
-# if player 1 has full list first lose 
-# if player 2 has full list first lose 
-
