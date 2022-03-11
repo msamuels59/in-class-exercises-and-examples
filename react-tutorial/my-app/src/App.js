bc8804f1444576e53495b3dfacbe99aa7377289f
Mike Samuels <msamuels59@gmail.com>
2022-03-11 17:59:11 -0500

    added nav

--- react-tutorial/my-app/src/App.js
+++ react-tutorial/my-app/src/App.js
@@ -3,16 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import Favorites from './pages/Favorites';
+import MainNavigation from './components/layouts/MainNavigation';
+
 
 
 function App() {
-  return <div>
-    <Routes>
-    <Route path='/' element={<AllMeetupsPage />} />
-    <Route path='/new-meetup' element={<NewMeetupsPage />} />
-    <Route path='/favorites' element={<Favorites />} />
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />} />
+        <Route path='/new-meetup' element={<NewMeetupsPage />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
