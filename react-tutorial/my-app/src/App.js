c2517317c751f1c8864b0c1a2914eaeb035302b2
Mike Samuels <msamuels59@gmail.com>
2022-03-14 21:29:16 -0400

    added layout update

--- react-tutorial/my-app/src/App.js
+++ react-tutorial/my-app/src/App.js
@@ -3,20 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import Favorites from './pages/Favorites';
-import MainNavigation from './components/layouts/MainNavigation';
+import Layout from './components/layouts/Layout';
 
 
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+      <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupsPage />} />
         <Route path='/favorites' element={<Favorites />} />
       </Routes>
-    </div>
+      </Layout>
   );
 }
 
