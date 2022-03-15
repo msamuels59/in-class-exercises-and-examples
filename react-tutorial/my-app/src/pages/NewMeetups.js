12b2722fb3b482b0468fdb592e4dfbec8d363d0d
Mike Samuels <msamuels59@gmail.com>
2022-03-14 22:55:16 -0400

    added redirect

--- react-tutorial/my-app/src/pages/NewMeetups.js
+++ react-tutorial/my-app/src/pages/NewMeetups.js
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
         fetch('https://react-app-d2f90-default-rtdb.firebaseio.com/meetups.json',
             {
@@ -10,7 +14,9 @@ function NewMeetupsPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            navigate('/', {replace: true});
+        });
     }
 
     return (
