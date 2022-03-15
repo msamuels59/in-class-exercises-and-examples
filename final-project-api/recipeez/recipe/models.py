3135f723177043c0410b949250ec10d4af58cf65
Mike Samuels <msamuels59@gmail.com>
2022-03-15 18:25:20 -0400

    api created

--- final-project-api/recipeez/recipe/models.py
+++ final-project-api/recipeez/recipe/models.py
@@ -1,3 +1,15 @@
+from distutils.text_file import TextFile
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=120)
+    type = models.CharField(max_length=120, blank=True, null=True)
+    ingredients = models.TextField(blank=True, null=True)
+    prep_minutes = models.IntegerField(blank=True, null=True)
+    number_served = models.IntegerField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
--- final-project-api/recipeez/recipe/views.py
+++ final-project-api/recipeez/recipe/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ReciperSerializer
+from .models import Recipe
 
-# Create your views here.
+class RecipeView(viewsets.ModelViewSet):
+    serializer_class = ReciperSerializer
+    queryset = Recipe.objects.all()
